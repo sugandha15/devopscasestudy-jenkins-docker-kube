326470fe60c02d4e33c5d2fb0feec67326dadc13
Sugandha15 <sugandha203@gmail.com>
2020-05-21 23:20:02 +0530

    first commit

---
+++ src/main/java/com/example/devopscasestudyjenkinsdockerkube/DevopscasestudyJenkinsDockerKubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopscasestudyjenkinsdockerkube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopscasestudyJenkinsDockerKubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopscasestudyJenkinsDockerKubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopscasestudyjenkinsdockerkube/DevopscasestudyJenkinsDockerKubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopscasestudyjenkinsdockerkube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopscasestudyJenkinsDockerKubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
